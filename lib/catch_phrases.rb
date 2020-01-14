2998be4683ef3b97ce93bb70fc71926cb57db921
Harley McDowell <hmcdowell95@gmail.com>
2020-01-14 00:48:31 +0000

    Automatically backed up by Learn

--- lib/catch_phrases.rb
+++ lib/catch_phrases.rb
@@ -1,8 +1,9 @@
+phrase = "It's-a me, Mario!"
 def mario
-  status = 'Thank You Mario! But Our Princess Is In Another Castle!'
+  $status = 'Thank You Mario! But Our Princess Is In Another Castle!'
   puts phrase
 end
 
 def toadstool
-  puts status
+  puts $status
 end
